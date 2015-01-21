92cf2d7e156f7e83c83e34aca68a20c9ec52c718
Vivek Shukla <vivek.f19@gmail.com>
2015-01-21 15:15:45 -0500

    Added 5% more to the cohorts total.

--- excercise7.rb
+++ excercise7.rb
@@ -16,3 +16,9 @@ display_students_in_diff_cohorts(students)
 
 #Excercise 7 Question 3
 puts "Cohort names: #{students.keys}"
+
+#Excercise 7 Question 4
+students.each {|key,value| students[key] = (1.05*value).to_i}
+display_students_in_diff_cohorts(students)
+
+#Excercise 7 Question 5
