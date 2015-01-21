2f9a16d015e7287ed5f6543d17650dcefb465f7d
Vivek Shukla <vivek.f19@gmail.com>
2015-01-21 14:55:39 -0500

    Added another cohort to the hash.

--- excercise7.rb
+++ excercise7.rb
@@ -3,9 +3,15 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
-
+#Excercise 7 Question 1
 def display_students_in_diff_cohorts(list)
 	list.each {|key,value| puts "#{key}: #{value} students."}
 end
 
 display_students_in_diff_cohorts(students)
+
+#Excercise 7 Question 2
+students[:cohort4] = 43
+display_students_in_diff_cohorts(students)
+
+#Excercise 7 Question 3
