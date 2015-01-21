88df95be7a81b34ab90ef8cfa776c71bcc0f234c
Vivek Shukla <vivek.f19@gmail.com>
2015-01-21 14:13:23 -0500

    Added a function that lists the items for me. Also added a 'puts' that displays the count.

--- excercise6.rb
+++ excercise6.rb
@@ -3,4 +3,17 @@ grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 #Exercise 6 Question 1
 grocery_list.each {|item| puts "*#{item}"}
 
-#Exercise 6 Question 1
+#Exercise 6 Question 2
+grocery_list.push("rice")
+#grocery_list.each {|item| puts "*#{item}"}
+
+def display_all_items(list)
+  list.each {|item| puts "*#{item}"}
+end
+
+display_all_items(grocery_list)
+
+#Exercise 6 Question 3
+puts "The total number of items on list: #{grocery_list.count}."
+
+#Exercise 6 Question 4
